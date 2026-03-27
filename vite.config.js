1fbeff45dda1daaefbb40eb58611e8710cdb07b5
karthikah0112 <karthikah999@gmail.com>
2026-03-27 17:01:31 +0000

    Use hashed build asset filenames to prevent stale cache

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,4 @@ const BASE_PATH = '/'
 export default defineConfig({
   plugins: [react()],
   base: BASE_PATH,
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/index.js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name][extname]',
-      },
-    },
-  },
 })
